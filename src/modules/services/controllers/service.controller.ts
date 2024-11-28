de6ee8b3d0068f5d2171d64563fa3e871320a583
_JMNRA <jmedinan@unsa.edu.pe>
2024-11-28 16:12:22 -0500

    feat: crud services añadidos

--- src/modules/services/controllers/service.controller.ts
+++ src/modules/services/controllers/service.controller.ts
@@ -1,14 +1,27 @@
-import { Body, Controller, Post, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ServiceService } from '../services/service.service';
 import { CreateServiceDto } from '../dto/create-service.dto';
 import { Auth, GetUser } from '@login/login/admin/auth/decorators';
-import { UserData } from '@login/login/interfaces';
+import { HttpResponse, UserData } from '@login/login/interfaces';
 import {
   ApiBadRequestResponse,
   ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
+import { UpdateServiceDto } from '../dto';
+import { Service } from '../entities/service.entity';
+import { DeleteServicesDto } from '../dto/delete-services.dto';
 
 /**
  * Controlador REST para gestionar servicios médicos
@@ -40,17 +53,53 @@ export class ServiceController {
   create(
     @Body() createServiceDto: CreateServiceDto,
     @GetUser() user: UserData,
-  ) {
+  ): Promise<HttpResponse<Service>> {
     return this.serviceService.create(createServiceDto, user);
   }
 
-  @Get()
-  findAll() {
-    return this.serviceService.findAll();
+  @Patch(':id')
+  @ApiOkResponse({ description: 'Design project updated successfully' })
+  @ApiBadRequestResponse({ description: 'Validation failed or bad request' })
+  update(
+    @Param('id') id: string,
+    @Body() updateServiceDto: UpdateServiceDto,
+    @GetUser() user: UserData,
+  ): Promise<HttpResponse<Service>> {
+    return this.serviceService.update(id, updateServiceDto, user);
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ description: 'Service deleted successfully' })
+  @ApiBadRequestResponse({ description: 'Validation failed or bad request' })
+  delete(
+    @Param('id') id: string,
+    @GetUser() user: UserData,
+  ): Promise<HttpResponse<Service>> {
+    return this.serviceService.delete(id, user);
+  }
+
+  @Delete('remove/all')
+  @ApiOkResponse({ description: 'Services deleted successfully' })
+  @ApiBadRequestResponse({ description: 'Validation failed or bad request' })
+  deleteMany(
+    @Body() deleteServicesDto: DeleteServicesDto,
+    @GetUser() user: UserData,
+  ) {
+    return this.serviceService.deleteMany(deleteServicesDto, user);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.serviceService.findById(id);
+  @ApiOkResponse({ description: 'Service found' })
+  @ApiNotFoundResponse({ description: 'Service not found' })
+  findOne(@Param('id') id: string): Promise<Service> {
+    return this.serviceService.findOne(id);
+  }
+
+  @Get()
+  @ApiOkResponse({
+    description: 'Get all services',
+  })
+  findAll(): Promise<Service[]> {
+    return this.serviceService.findAll();
   }
 }
