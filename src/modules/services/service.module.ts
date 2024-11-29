08fd2ae05c50e83118f25bb5f2f5d467eaebdba0
_JMNRA <jmedinan@unsa.edu.pe>
2024-11-29 17:30:53 -0500

    feat(services): añadir providers a module

--- src/modules/services/service.module.ts
+++ src/modules/services/service.module.ts
@@ -10,6 +10,9 @@ import { CreateServiceUseCase } from './use-cases/create-service.use-case';
 import { UpdateServiceUseCase } from './use-cases/update-service.use-case';
 import { DeleteServiceUseCase } from './use-cases/delete-service.use-case';
 import { DeleteServicesUseCase } from './use-cases/delete-services.use-case';
+import { CreateServiceTypeUseCase } from './use-cases/create-servicetype.use-case';
+import { UpdateServiceTypeUseCase } from './use-cases/update-servicetype.use-case';
+import { DeleteServiceTypeUseCase } from './use-cases/delete-servicetype.use-case';
 
 /**
  * Módulo que gestiona los servicios y tipos de servicios del sistema.
@@ -27,6 +30,9 @@ import { DeleteServicesUseCase } from './use-cases/delete-services.use-case';
     UpdateServiceUseCase,
     DeleteServiceUseCase,
     DeleteServicesUseCase,
+    CreateServiceTypeUseCase,
+    UpdateServiceTypeUseCase,
+    DeleteServiceTypeUseCase,
   ],
   controllers: [ServiceController, ServiceTypeController],
   exports: [ServiceService, ServiceTypeService],
