231da6f0cb5b2a0daa02293e31420d364e124d9c
_JMNRA <jmedinan@unsa.edu.pe>
2024-11-29 17:30:11 -0500

    feat(services): añadir endpoint update

--- src/modules/services/controllers/service-type.controller.ts
+++ src/modules/services/controllers/service-type.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Get,
+  Delete,
+} from '@nestjs/common';
 import { ServiceTypeService } from '../services/service-type.service';
 import { CreateServiceTypeDto } from '../dto/create-service-type.dto';
 import {
   ApiBadRequestResponse,
   ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { Auth, GetUser } from '@login/login/admin/auth/decorators';
 import { ServiceType } from '../entities/service.entity';
 import { HttpResponse, UserData } from '@login/login/interfaces';
+import { UpdateServiceTypeDto } from '../dto';
 
 @ApiTags('ServiceTypes')
 @ApiBadRequestResponse({ description: 'Bad Request' })
@@ -31,5 +42,40 @@ export class ServiceTypeController {
     @GetUser() user: UserData,
   ): Promise<HttpResponse<ServiceType>> {
     return this.serviceTypeService.create(createServiceTypeDto, user);
+  }
+
+  @Patch(':id')
+  @ApiOkResponse({ description: 'Design project updated successfully' })
+  update(
+    @Param('id') id: string,
+    @Body() updateServiceDto: UpdateServiceTypeDto,
+    @GetUser() user: UserData,
+  ): Promise<HttpResponse<ServiceType>> {
+    return this.serviceTypeService.update(id, updateServiceDto, user);
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ description: 'ServiceType found' })
+  @ApiNotFoundResponse({ description: 'ServiceType not found' })
+  findOne(@Param('id') id: string): Promise<ServiceType> {
+    return this.serviceTypeService.findOne(id);
+  }
+
+  @Get()
+  @ApiOkResponse({
+    description: 'Get all services types',
+  })
+  findAll(): Promise<ServiceType[]> {
+    return this.serviceTypeService.findAll();
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ description: 'ServiceType deleted successfully' })
+  @ApiBadRequestResponse({ description: 'Validation failed or bad request' })
+  delete(
+    @Param('id') id: string,
+    @GetUser() user: UserData,
+  ): Promise<HttpResponse<ServiceType>> {
+    return this.serviceTypeService.delete(id, user);
   }
 }
