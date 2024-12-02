a40450ca0bc8095492459231f36da52c5a6bbacf
Alex <worfysito15@gmail.com>
2024-12-02 09:24:32 -0500

    feat: actualizar configuración y nombres para la aplicación Juan Pablo II

--- libs/login/src/email/email.module.ts
+++ libs/login/src/email/email.module.ts
@@ -23,7 +23,7 @@ import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
           },
         },
         defaults: {
-          from: 'Trazo',
+          from: 'Juan Pablo II',
         },
         template: {
           dir: join(
