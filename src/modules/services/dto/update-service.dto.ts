1319ac998c803b9b545f3fd580657155803b0a12
_JMNRA <jmedinan@unsa.edu.pe>
2024-12-03 12:12:56 -0500

    chore(services): añádir validacion en dtos

--- src/modules/services/dto/update-service.dto.ts
+++ src/modules/services/dto/update-service.dto.ts
@@ -1,47 +1,4 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateServiceDto } from './create-service.dto';
-import {
-  IsString,
-  IsNumber,
-  IsUUID,
-  IsOptional,
-  MinLength,
-  Min,
-} from 'class-validator';
 
-export class UpdateServiceDto extends PartialType(CreateServiceDto) {
-  @ApiPropertyOptional({
-    description: 'Name of the service',
-    example: 'Consultation',
-  })
-  @IsString()
-  @MinLength(2)
-  @IsOptional()
-  name?: string;
-
-  @ApiPropertyOptional({
-    description: 'Description of the service',
-    example: 'Medical consultation with a specialist',
-  })
-  @IsString()
-  @IsOptional()
-  description?: string;
-
-  @ApiPropertyOptional({
-    description: 'Price of the service',
-    example: 50.0,
-    minimum: 0,
-  })
-  @IsNumber()
-  @Min(0)
-  @IsOptional()
-  price?: number;
-
-  @ApiPropertyOptional({
-    description: 'ID of the service type',
-  })
-  @IsUUID()
-  @IsOptional()
-  serviceTypeId?: string;
-}
+export class UpdateServiceDto extends PartialType(CreateServiceDto) {}
