ef731977cc7594f3aca28fa2451f36258d28dfc1
_JMNRA <jmedinan@unsa.edu.pe>
2024-12-16 12:13:36 -0500

    fix: billing service corregido para usar usecase

--- src/modules/billing/services/billing.service.ts
+++ src/modules/billing/services/billing.service.ts
@@ -3,17 +3,26 @@ import { ServiceBillingGenerator } from '../generators/service-billing.generator
 import { CreateServiceBillingDto } from '../dto/create-service-billing.dto';
 import { UserData } from '@login/login/interfaces';
 import { OrderService } from '@pay/pay/services/order.service';
+import { CreateServiceBillingUseCase } from '../use-cases/create-service-billing.use-case';
+import { BaseErrorHandler } from 'src/common/error-handlers/service-error.handler';
 
 @Injectable()
 export class BillingService {
+  private readonly errorHandler: BaseErrorHandler;
+
   constructor(
     private readonly orderService: OrderService,
     private readonly serviceBillingGenerator: ServiceBillingGenerator,
+    private readonly createServiceBillingUseCase: CreateServiceBillingUseCase,
   ) {
     this.orderService.registerGenerator(this.serviceBillingGenerator);
   }
 
   async createServiceBilling(dto: CreateServiceBillingDto, user: UserData) {
-    return this.orderService.createOrder('SERVICE_BILLING', dto, user);
+    try {
+      await this.createServiceBillingUseCase.execute(dto, user);
+    } catch (error) {
+      this.errorHandler.handleError(error, 'creating');
+    }
   }
 }
--- src/modules/billing/use-cases/create-service-billing.use-case.ts
+++ src/modules/billing/use-cases/create-service-billing.use-case.ts
@@ -2,18 +2,23 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 import { AuditService } from '@login/login/admin/audit/audit.service';
 import { UserData } from '@login/login/interfaces';
 import { AuditActionType } from '@prisma/client';
-import { BillingService } from '../services/billing.service';
 import { CreateServiceBillingDto } from '../dto/create-service-billing.dto';
+import { OrderService } from '@pay/pay/services/order.service';
 
 @Injectable()
 export class CreateServiceBillingUseCase {
   constructor(
-    private readonly billingService: BillingService,
     private readonly auditService: AuditService,
+    private readonly orderService: OrderService,
   ) {}
 
   async execute(dto: CreateServiceBillingDto, user: UserData) {
-    const order = await this.billingService.createServiceBilling(dto, user);
+    const order = await this.orderService.createOrder(
+      'SERVICE_BILLING',
+      dto,
+      user,
+    );
+    //const order = await this.billingService.createServiceBilling(dto, user);
 
     // Registrar auditoría
     await this.auditService.create({
