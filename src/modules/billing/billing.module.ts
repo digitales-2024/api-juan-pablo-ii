22bb423a1a9b2c2bf4311ae1ab3ca8c426e1c33c
_JMNRA <jmedinan@unsa.edu.pe>
2024-12-18 13:32:10 -0500

    feat(billing): añadir importaciones a module

--- src/modules/billing/billing.module.ts
+++ src/modules/billing/billing.module.ts
@@ -5,14 +5,22 @@ import { AuditModule } from '@login/login/admin/audit/audit.module';
 import { PayModule } from '@pay/pay/pay.module';
 import { MedicalConsultationGenerator } from './generators/medical-consultation.generator';
 import { CreateMedicalConsultationOrderUseCase } from './use-cases/create-medical-consultation-billing.use-case';
+import { CreateMedicalPrescriptionOrderUseCase } from './use-cases/create-medical-prescription-billing.use-case';
+import { MedicalPrescriptionGenerator } from './generators/medical-prescription.generator';
+import { ProductSaleGenerator } from './generators/product-sale-generator';
+import { CreateProductSaleOrderUseCase } from './use-cases/create-product-sale-billing.use-case';
 
 @Module({
   imports: [PayModule, AuditModule],
   controllers: [BillingController],
   providers: [
     MedicalConsultationGenerator,
+    MedicalPrescriptionGenerator,
+    ProductSaleGenerator,
     BillingService,
     CreateMedicalConsultationOrderUseCase,
+    CreateMedicalPrescriptionOrderUseCase,
+    CreateProductSaleOrderUseCase,
   ],
 })
 export class BillingModule {}
