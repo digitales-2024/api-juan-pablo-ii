32266020c7b768983171c2376abbc5c2baf7fa0b
EleoRomario <eleo.romario@gmail.com>
2025-01-17 09:17:51 -0500

    feat: agregar DTO UserResponseDto con propiedades de estado y última conexión

---
+++ libs/login/src/admin/users/dto/user-response.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { UserProfileResponseDto } from './user-profile-reponse.dto';
+import { IsBoolean, IsString } from 'class-validator';
+
+export class UserResponseDto extends UserProfileResponseDto {
+  @ApiProperty({
+    description: 'El usuario está activa',
+    example: true,
+    type: Boolean,
+  })
+  @IsBoolean()
+  isActive: boolean;
+
+  @ApiProperty({
+    description: 'El usuario debe cambiar la contraseña',
+    example: true,
+    type: Boolean,
+  })
+  @IsBoolean()
+  mustChangePassword: boolean;
+
+  @ApiProperty({
+    description: 'Última vez que el usuario hizo login',
+    example: true,
+    type: Boolean,
+  })
+  @IsString()
+  lastLogin: Date;
+}
