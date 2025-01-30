d4610555991cdc47e12d3b1ce7df63ba4dba79fc
alxFv <worfysito15@gmail.com>
2025-01-30 17:32:43 -0500

    feat: cambios detectados nueva funcionalidad en delete-up-history.dto.ts

--- libs/pacient/src/update-history/dto/delete-up-history.dto.ts
+++ libs/pacient/src/update-history/dto/delete-up-history.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsString } from 'class-validator';
 
-export class DeleteUpHistoryDto {
+export class DeleteUpdateHistoryDto {
   @ApiProperty({ type: [String] })
   @IsArray()
   @IsString({ each: true })
