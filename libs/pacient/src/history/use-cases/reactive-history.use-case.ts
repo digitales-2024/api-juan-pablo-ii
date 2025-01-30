74820026a51696fcf5aa605e67a1deca81f1d13c
alxFv <worfysito15@gmail.com>
2025-01-30 17:32:42 -0500

    feat: cambios detectados nueva funcionalidad en reactive-history.use-case.ts

--- libs/pacient/src/history/use-cases/reactive-history.use-case.ts
+++ libs/pacient/src/history/use-cases/reactive-history.use-case.ts
@@ -1,32 +1,34 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
-import { HistoryRepository } from '../repositories/history.repository';
-import { History } from '../entities/history.entity';
-import { HttpResponse, UserData } from '@login/login/interfaces';
+import { Injectable } from '@nestjs/common';
+import { MedicalHistoryRepository } from '../repositories/history.repository';
+import { MedicalHistory } from '../entities/history.entity';
+import { UserData } from '@login/login/interfaces';
 import { AuditService } from '@login/login/admin/audit/audit.service';
 import { AuditActionType } from '@prisma/client';
+import { BaseApiResponse } from 'src/dto/BaseApiResponse.dto';
 
 @Injectable()
-export class ReactivateHistoryUseCase {
+export class ReactivateMedicalHistoryUseCase {
   constructor(
-    private readonly historyRepository: HistoryRepository,
+    private readonly medicalHistoryRepository: MedicalHistoryRepository,
     private readonly auditService: AuditService,
   ) {}
 
   async execute(
     ids: string[],
     user: UserData,
-  ): Promise<HttpResponse<History[]>> {
+  ): Promise<BaseApiResponse<MedicalHistory[]>> {
     // Reactivar las historias y registrar auditoría
-    const reactivatedHistories = await this.historyRepository.transaction(
-      async () => {
-        const histories = await this.historyRepository.reactivateMany(ids);
+    const reactivatedHistories =
+      await this.medicalHistoryRepository.transaction(async () => {
+        const histories =
+          await this.medicalHistoryRepository.reactivateMany(ids);
 
         // Registrar auditoría para cada historia reactivada
         await Promise.all(
           histories.map((history) =>
             this.auditService.create({
               entityId: history.id,
-              entityType: 'historiaMedica',
+              entityType: 'medicalHistory',
               action: AuditActionType.UPDATE,
               performedById: user.id,
               createdAt: new Date(),
@@ -35,11 +37,10 @@ export class ReactivateHistoryUseCase {
         );
 
         return histories;
-      },
-    );
+      });
 
     return {
-      statusCode: HttpStatus.OK,
+      success: true,
       message: 'Historias médicas reactivadas exitosamente',
       data: reactivatedHistories,
     };
