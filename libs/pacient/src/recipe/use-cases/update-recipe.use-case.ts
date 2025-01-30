07c1ef37ea4c641288a19a55b9164aaf0432b66c
alxFv <worfysito15@gmail.com>
2025-01-30 17:32:43 -0500

    feat: cambios detectados nueva funcionalidad en update-recipe.use-case.ts

--- libs/pacient/src/recipe/use-cases/update-recipe.use-case.ts
+++ libs/pacient/src/recipe/use-cases/update-recipe.use-case.ts
@@ -1,52 +1,56 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
-import { UpdateRecipeDto } from '../dto/update-recipe.dto';
-import { Recipe } from '../entities/recipe.entity';
-import { RecipeRepository } from '../repositories/recipe.repository';
-import { HttpResponse, UserData } from '@login/login/interfaces';
+import { Injectable } from '@nestjs/common';
+import { UpdatePrescriptionDto } from '../dto/update-recipe.dto';
+import { Prescription } from '../entities/recipe.entity';
+import { PrescriptionRepository } from '../repositories/recipe.repository';
+import { UserData } from '@login/login/interfaces';
 import { AuditService } from '@login/login/admin/audit/audit.service';
 import { AuditActionType } from '@prisma/client';
+import { BaseApiResponse } from 'src/dto/BaseApiResponse.dto';
 
 @Injectable()
-export class UpdateRecipeUseCase {
+export class UpdatePrescriptionUseCase {
   constructor(
-    private readonly recipeRepository: RecipeRepository,
+    private readonly prescriptionRepository: PrescriptionRepository,
     private readonly auditService: AuditService,
   ) {}
 
   async execute(
     id: string,
-    updateRecipeDto: UpdateRecipeDto,
+    updatePrescriptionDto: UpdatePrescriptionDto,
     user: UserData,
-  ): Promise<HttpResponse<Recipe>> {
-    const updatedRecipe = await this.recipeRepository.transaction(async () => {
-      // Update recipe
-      const recipe = await this.recipeRepository.update(id, {
-        updateHistoriaId: updateRecipeDto.updateHistoriaId,
-        sucursalId: updateRecipeDto.sucursalId,
-        personalId: updateRecipeDto.personalId,
-        pacienteId: updateRecipeDto.pacienteId,
-        fechaRegistro: updateRecipeDto.fechaRegistro,
-        receta: updateRecipeDto.receta,
-        descripcion: updateRecipeDto.descripcion,
-        ordenCompraId: updateRecipeDto.ordenCompraId,
-      });
+  ): Promise<BaseApiResponse<Prescription>> {
+    const updatedPrescription = await this.prescriptionRepository.transaction(
+      async () => {
+        // Update prescription
+        const prescription = await this.prescriptionRepository.update(id, {
+          updateHistoryId: updatePrescriptionDto.updateHistoryId,
+          branchId: updatePrescriptionDto.branchId,
+          staffId: updatePrescriptionDto.staffId,
+          patientId: updatePrescriptionDto.patientId,
+          registrationDate: updatePrescriptionDto.registrationDate,
+          prescription: updatePrescriptionDto.prescription,
+          description: updatePrescriptionDto.description,
+          purchaseOrderId: updatePrescriptionDto.purchaseOrderId,
+          isActive: true,
+        });
 
-      // Register audit
-      await this.auditService.create({
-        entityId: recipe.id,
-        entityType: 'receta',
-        action: AuditActionType.UPDATE,
-        performedById: user.id,
-        createdAt: new Date(),
-      });
+        // Register audit
+        await this.auditService.create({
+          entityId: prescription.id,
+          entityType: 'prescription',
+          action: AuditActionType.UPDATE,
+          performedById: user.id,
+          createdAt: new Date(),
+        });
 
-      return recipe;
-    });
+        return prescription;
+      },
+    );
 
     return {
-      statusCode: HttpStatus.OK,
+      success: true,
       message: 'Receta médica actualizada exitosamente',
-      data: updatedRecipe,
+      data: updatedPrescription,
     };
   }
 }
