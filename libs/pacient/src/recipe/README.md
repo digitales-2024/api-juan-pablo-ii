2247afcd3f53c26481fc75fd5c19798b395de306
JMNRA <jmedinan@unsa.edu.pe>
2025-02-04 10:21:59 -0500

    feat: actualizar exportaciones de DTOs y casos de uso en módulos de paciente y receta, y agregar archivo de configuración TypeScript

---
+++ libs/pacient/src/recipe/README.md
@@ -0,0 +1,8 @@
+
+```bash
+
+# Libreria de recetea medica se gestiona la receta medica y el descanso medico para el paciente.
+
+#flujo grama : https://miro.com/app/board/uXjVLCVDyns=/?share_link_id=58779936518
+
+```
--- libs/pacient/src/recipe/use-cases/index.ts
+++ libs/pacient/src/recipe/use-cases/index.ts
@@ -1,4 +1,4 @@
 export * from './create-recipe.use-case';
+export * from './update-recipe.use-case';
 export * from './delete-recipe.use-case';
 export * from './reactive-recipe.use-case';
-export * from './update-recipe.use-case';
